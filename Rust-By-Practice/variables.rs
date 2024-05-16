fd6620ea200fcb78c36197a8cd5ab8d3ddd52eca
Musab Habeeb <habeebmusab@gmail.com>
2024-05-16 21:41:10 +0100

    done with variables

--- Rust-By-Practice/variables.rs
+++ Rust-By-Practice/variables.rs
@@ -95,3 +95,30 @@ fn main() {
 fn main() {
     let x = 1;
 }
+
+
+// 8
+
+// Fix the error below with least amount of modification
+fn main() {
+    let (mut x, y) = (1, 2);
+    x += 2;
+
+    assert_eq!(x, 3);
+    assert_eq!(y, 2);
+
+    println!("Success!");
+}
+
+
+// 9
+
+fn main() {
+    let (x, y);
+    (x,..) = (3, 4);
+    [.., y] = [1, 2];
+    // Fill the blank to make the code work
+    assert_eq!([x,y], [3, 2]);
+
+    println!("Success!");
+} 
