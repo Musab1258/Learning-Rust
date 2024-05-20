cd1c1d0028ed69a95fa19b63f6a706c1c6bd1990
Musab Habeeb <habeebmusab@gmail.com>
2024-05-20 07:33:22 +0100

    done with ownership

--- Rust-By-Practice/ownership-borrowing/ownership.rs
+++ Rust-By-Practice/ownership-borrowing/ownership.rs
@@ -51,7 +51,6 @@ fn take_ownership(s: String) -> String {
 
 
 // 3
-
 fn main() {
     let s = give_ownership();
     println!("{}", s);
@@ -64,5 +63,112 @@ fn give_ownership() -> String {
     s
 }
 
+or
+
+
+fn main() {
+    let s = give_ownership();
+    println!("{}", s);
+}
+
+// Only modify the code below!
+fn give_ownership() -> String {
+    let s = String::from("Hello world");
+    s
+}
+
 
 // 4
+fn main() {
+    let s = String::from("Hello World");
+
+    print_str(s.clone());
+
+    println!("{}", s);
+}
+
+fn print_str(s: String)  {
+    println!("{}",s);
+}
+
+or
+
+fn main() {
+    let s = String::from("Hello World");
+
+    print_str(&s);
+
+    println!("{}", s);
+}
+
+fn print_str(s: &String)  {
+    println!("{}",s);
+}
+
+
+// 5
+fn main() {
+    let x = (1, 2, (), "hello");
+    let y = x;
+    println!("{:?}, {:?}", x, y);
+}
+
+
+// 6
+fn main() {
+    let s = String::from("Hello ");
+    
+    let mut s1 = s;
+
+    s1.push_str("World!");
+
+    println!("Success!");
+}
+
+
+// 7
+fn main() {
+    let x = Box::new(5);
+    
+    let mut y = Box::new(2);      // update this line, don't change other lines!
+    
+    *y = 4;
+    
+    assert_eq!(*x, 5);
+
+    println!("Success!");
+}
+
+
+// 8
+fn main() {
+    let t = (String::from("hello"), String::from("world"));
+ 
+    let _s = t.0;
+ 
+    // Modify this line only, don't use `_s`
+    println!("{:?}", t.1);
+ }
+
+
+// 9 
+
+fn main() {
+    let t = (String::from("hello"), String::from("world"));
+ 
+     // Fill the blanks
+     let (s1, s2) = t.clone();
+ 
+     println!("{:?}, {:?}, {:?}", s1, s2, t); // -> "hello", "world", ("hello", "world")
+}
+
+or
+
+fn main() {
+    let t = (String::from("hello"), String::from("world"));
+ 
+     // Fill the blanks
+     let (s1, s2) = (&t.0, &t.1);
+ 
+     println!("{:?}, {:?}, {:?}", s1, s2, t); // -> "hello", "world", ("hello", "world")
+}
