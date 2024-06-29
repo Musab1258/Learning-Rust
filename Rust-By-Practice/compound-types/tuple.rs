177ce75c49eb14813aef060a69eafbd035a6203f
Musab Habeeb <habeebmusab@gmail.com>
2024-06-30 00:29:43 +0100

    done with tuples

--- Rust-By-Practice/compound-types/tuple.rs
+++ Rust-By-Practice/compound-types/tuple.rs
@@ -27,4 +27,46 @@ fn main() {
 
 
 // 4
+fn main() {
+    let tup = (1, 6.4, "hello");
 
+    // Fill the blank to make the code work
+    let (x, z, y) = tup;
+
+    assert_eq!(x, 1);
+    assert_eq!(y, "hello");
+    assert_eq!(z, 6.4);
+
+    println!("Success!");
+}
+
+
+// 5
+fn main() {
+    let (x, y, z);
+
+    // Fill the blank
+    (y, z, x) = (1, 2, 3);
+    
+    assert_eq!(x, 3);
+    assert_eq!(y, 1);
+    assert_eq!(z, 2);
+
+    println!("Success!");
+}
+
+
+// 6
+fn main() {
+    // Fill the blank, need a few computations here.
+    let (x, y) = sum_multiply((2, 3));
+
+    assert_eq!(x, 5);
+    assert_eq!(y, 6);
+
+    println!("Success!");
+}
+
+fn sum_multiply(nums: (i32, i32)) -> (i32, i32) {
+    (nums.0 + nums.1, nums.0 * nums.1)
+}
